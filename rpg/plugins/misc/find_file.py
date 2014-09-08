ef560c92d66d00f62764c809890fa769134e3121
Jan Silhan <jsilhan@redhat.com>
2014-09-08 16:38:29 +0200

    find_file plugin - fix (#46)

--- rpg/plugins/misc/find_file.py
+++ rpg/plugins/misc/find_file.py
@@ -4,4 +4,5 @@ from rpg.plugin import Plugin
 class FindFilePlugin(Plugin):
 
     def compiled(self, project_dir, spec, sack):
-        pass
+        for item in list(project_dir.glob('**/*.*')):
+            spec.files.append([item.relative_to(project_dir), None, None])
