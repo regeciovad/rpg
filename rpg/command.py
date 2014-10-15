4cbbb90941f2e20119766a2b0ddc217cf6134977
shootervm <shooter.vm@gmail.com>
2014-10-15 19:24:12 +0200

    command: cmd_output stderr and stdout redirected to /dev/null (fix #72)

--- rpg/command.py
+++ rpg/command.py
@@ -59,6 +59,6 @@ class Command:
 
 
 def cmd_output(cmdlines):
-    output = check_output(["/bin/sh", "-c", " && ".join(cmdlines)])
+    output = check_output(["/bin/sh", "-c", " && ".join(cmdlines], stderr=None)
     # TODO don't print stdout of commands
     return output.decode('utf-8')
